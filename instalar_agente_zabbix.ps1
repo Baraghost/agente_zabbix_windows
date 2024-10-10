2af4934bc4cbb800e21eeb61fb897196ab51d46e
Baraghost <soporte@baraghost.com>
2024-10-10 07:58:45 -0300

    Update instalar_agente_zabbix.ps1
    Yellow to DarkYellow

--- instalar_agente_zabbix.ps1
+++ instalar_agente_zabbix.ps1
@@ -14,7 +14,7 @@ if (-not (Test-Path -Path $zabbixFolder)) {
 }
 
 # Descargar el archivo ZIP del agente
-Write-Host "Descargando Zabbix Agent desde $zabbixZipUrl..." -ForegroundColor Yellow
+Write-Host "Descargando Zabbix Agent desde $zabbixZipUrl..." -ForegroundColor DarkYellow
 try {
     Invoke-WebRequest -Uri $zabbixZipUrl -OutFile $zabbixZipPath -ErrorAction Stop
     Write-Host "Descarga completada: $zabbixZipPath" -ForegroundColor Green
@@ -24,7 +24,7 @@ try {
 }
 
 # Extraer el contenido del ZIP a la carpeta de Zabbix
-Write-Host "Extrayendo Zabbix Agent en $zabbixFolder..." -ForegroundColor Yellow
+Write-Host "Extrayendo Zabbix Agent en $zabbixFolder..." -ForegroundColor DarkYellow
 try {
     Expand-Archive -Path $zabbixZipPath -DestinationPath $zabbixFolder -Force
     Write-Host "Extracción completada." -ForegroundColor Green
@@ -40,10 +40,10 @@ if (-not (Test-Path -Path $zabbixConf)) {
 }
 
 # Solicitar la IP del Servidor Zabbix
-$ServerIp = Read-Host "Ingrese la IP del servidor Zabbix" -ForegroundColor Yellow
+$ServerIp = Read-Host "Ingrese la IP del servidor Zabbix" -ForegroundColor DarkYellow
 
 # Editar el archivo de configuración para agregar el Server y LogFile
-Write-Host "Editando el archivo de configuración..." -ForegroundColor Yellow
+Write-Host "Editando el archivo de configuración..." -ForegroundColor DarkYellow
 try {
     # Reemplazar la línea de LogFile
     (Get-Content -Path $zabbixConf) -replace "^LogFile=.*", "LogFile=$logFilePath" | Set-Content -Path $zabbixConf
@@ -58,7 +58,7 @@ try {
 }
 
 # Instalar el agente Zabbix
-Write-Host "Instalando Zabbix Agent..." -ForegroundColor Yellow
+Write-Host "Instalando Zabbix Agent..." -ForegroundColor DarkYellow
 try {
     Start-Process -FilePath "$zabbixFolder\bin\zabbix_agentd.exe" -ArgumentList "--config `"$zabbixConf`" --install" -NoNewWindow -Wait
     Write-Host "Zabbix Agent instalado correctamente." -ForegroundColor Green
@@ -68,7 +68,7 @@ try {
 }
 
 # Iniciar el agente Zabbix
-Write-Host "Iniciando Zabbix Agent..." -ForegroundColor Yellow
+Write-Host "Iniciando Zabbix Agent..." -ForegroundColor DarkYellow
 try {
     Start-Process -FilePath "$zabbixFolder\bin\zabbix_agentd.exe" -ArgumentList "--config `"$zabbixConf`" --start" -NoNewWindow -Wait
     Write-Host "Zabbix Agent iniciado correctamente." -ForegroundColor Green
@@ -78,7 +78,7 @@ try {
 }
 
 # Configurar las reglas del firewall
-Write-Host "Configurando reglas de firewall..." -ForegroundColor Yellow
+Write-Host "Configurando reglas de firewall..." -ForegroundColor DarkYellow
 $firewallRuleName = "Zabbix Agent"
 $existingRule = Get-NetFirewallRule -DisplayName $firewallRuleName -ErrorAction SilentlyContinue
 
@@ -95,7 +95,7 @@ if ($existingRule) {
 }
 
 # Crear archivo ZabbixAgent.bat para reiniciar el agente
-Write-Host "Creando archivo ZabbixAgent.bat para reiniciar el agente..." -ForegroundColor Yellow
+Write-Host "Creando archivo ZabbixAgent.bat para reiniciar el agente..." -ForegroundColor DarkYellow
 $batContent = @"
 @echo off
 C:\zabbix\bin\zabbix_agentd.exe --config C:\zabbix\conf\zabbix_agentd.conf --stop
@@ -114,7 +114,7 @@ try {
 }
 
 # Crear una tarea programada para reiniciar el agente cada 2 horas
-Write-Host "Creando tarea programada para reiniciar el agente Zabbix cada 2 horas..." -ForegroundColor Yellow
+Write-Host "Creando tarea programada para reiniciar el agente Zabbix cada 2 horas..." -ForegroundColor DarkYellow
 $taskName = "Zabbix Service Restart"
 
 # Verificar si la tarea ya existe
